7d7bd633c25bce1972f1c7b1782926ad18464374
masoudjaliliaziz <masoudjalili0781@gmail.com>
2025-01-12 12:49:46 +0330

    adding redux toolkit to upgrade the classic redux to modern redux

--- src/store.js
+++ src/store.js
@@ -1,15 +1,11 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
-import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "@redux-devtools/extension";
+import { configureStore } from "@reduxjs/toolkit";
 //producing root reducer to pass to the store by combining our reducers----------
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
 export default store;
