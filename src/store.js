9bbf1f8e738dd4159305fa1dce1e2ecc64a9e674
masoudjaliliaziz <masoudjalili0781@gmail.com>
2025-01-12 08:10:47 +0330

    adding the new reducer for customers and learn about combinig reducers and pass the root reducer to the createStore classic redux

--- src/store.js
+++ src/store.js
@@ -1,11 +1,35 @@
-import { createStore } from "redux";
-const initialState = {
+import { combineReducers, createStore } from "redux";
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
 
-function reducer(state = initialState, action) {
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateName":
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
       return { ...state, balance: state.balance + action.payload };
@@ -31,10 +55,10 @@ function reducer(state = initialState, action) {
   }
 }
 
+//action creator for accountReducer---------------
 function deposit(amount) {
   return { type: "account/deposit", payload: amount };
 }
-
 function withdraw(amount) {
   return { type: "account/withdraw", payload: amount };
 }
@@ -51,7 +75,33 @@ function payLoan() {
   return { type: "account/payLoan" };
 }
 
-const store = createStore(reducer);
+//action creator for customerReducer---------------
+function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: {
+      fullName,
+      nationalID,
+      createdAt: new Date().toISOString(),
+    },
+  };
+}
+function updateName(fullName) {
+  return {
+    type: "customer/updateName",
+    payload: fullName,
+  };
+}
+
+//producing root reducer to pass to the store by combining our reducers----------
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+const store = createStore(rootReducer);
+
+//test--------------
+
 store.dispatch(deposit(1000));
 console.log(store.getState());
 store.dispatch(withdraw(200));
@@ -59,4 +109,7 @@ console.log(store.getState());
 store.dispatch(requestLoan(800, "bus KS car"));
 console.log(store.getState());
 store.dispatch(payLoan());
+store.dispatch(createCustomer("masoud jalili", "3980403955"));
+console.log(store.getState());
+store.dispatch(updateName("ali jalili"));
 console.log(store.getState());
