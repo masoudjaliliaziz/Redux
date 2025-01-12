35f954815a2760af7112c79b7013a88f34ef1090
masoudjaliliaziz <masoudjalili0781@gmail.com>
2025-01-12 07:05:52 +0330

    setting up the mini project and reducer function of our redux styate managment

---
+++ src/store.js
@@ -0,0 +1,26 @@
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/reqiestLoan":
+      if (state.loan > 0) return state;
+      return { ...state, loan: action.payload };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        balance: state.balance - state.loan,
+        loanPurpose: "",
+      };
+    default:
+      return state;
+  }
+}
