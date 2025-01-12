0f6bf2780c27f5dcd4eadba1494bb9ff33805a7b
masoudjaliliaziz <masoudjalili0781@gmail.com>
2025-01-12 07:43:14 +0330

    adding the action creator functions to the mini project

--- src/store.js
+++ src/store.js
@@ -31,18 +31,32 @@ function reducer(state = initialState, action) {
   }
 }
 
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: {
+      amount: amount,
+      purpose: purpose,
+    },
+  };
+}
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
 const store = createStore(reducer);
-store.dispatch({ type: "account/deposit", payload: 500 });
+store.dispatch(deposit(1000));
 console.log(store.getState());
-store.dispatch({ type: "account/withdraw", payload: 100 });
+store.dispatch(withdraw(200));
 console.log(store.getState());
-store.dispatch({
-  type: "account/requestLoan",
-  payload: {
-    amount: 1000,
-    purpose: "buy a car",
-  },
-});
+store.dispatch(requestLoan(800, "bus KS car"));
 console.log(store.getState());
-store.dispatch({ type: "account/payLoan" });
+store.dispatch(payLoan());
 console.log(store.getState());
